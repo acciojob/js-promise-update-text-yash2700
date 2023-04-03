c15984f863fc46fa5cc7755aac50577b7c53c086
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-04-03 09:10:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myPromise=new Promise((resolve,reject)=>{
-	setTimeout(()=>{resolve("Hello, world")},1000)
+	setTimeout(()=>{resolve("Hello, world!")},1000)
 })
 myPromise.then((value)=>{
 	document.getElementById("output").innerHTML=value
