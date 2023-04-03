9605f240873975c2ca2a0326c2bb034b54e91ef2
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-04-03 09:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var resolveAfter=(value,delay)=>
+	setTimeout(()=>{
+		return new Promise(resolve(value))
+		
+	},1000);
+
+document.getElementById("output").innerHTML=resolveAfter("Hello, world!",1000);
