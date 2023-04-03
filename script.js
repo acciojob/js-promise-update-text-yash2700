46b35328bb4274686a37b570591d42c09905bc1a
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-04-03 09:06:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-//your JS code here. If required.
-var resolveAfter=(value,delay)=>
-	setTimeout(()=>{
-		return new Promise(resolve(value))
-		
-	},1000);
-
-document.getElementById("output").innerHTML=resolveAfter("Hello, world!",1000);
+let myPromise=new Promise((resolve,reject)=>{
+	setTimeout(()=>{resolve("Hello, world")},100)
+})
+myPromise.then((value)=>{
+	document.getElementById("output").innerHTML=value
+})
